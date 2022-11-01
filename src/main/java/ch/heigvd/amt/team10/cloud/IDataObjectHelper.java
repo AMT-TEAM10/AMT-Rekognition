1549218b9d0d96a29cd64f8c9c45bc6355aa4bd0
Nicolas Crausaz <n.crausaz99@gmail.com>
2022-11-01 16:16:36 +0100

    Tests & impl of dataObject

--- src/main/java/ch/heigvd/amt/team10/cloud/IDataObjectHelper.java
+++ src/main/java/ch/heigvd/amt/team10/cloud/IDataObjectHelper.java
@@ -1,9 +1,12 @@
 package ch.heigvd.amt.team10.cloud;
 
+import software.amazon.awssdk.core.ResponseBytes;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+
 import java.io.File;
 
 public interface IDataObjectHelper {
-    void get(String objectName);
+    byte[] get(String objectName);
     void create(String objectName, File file);
     void update(String objectName, File newFile);
     void delete(String objectName);
