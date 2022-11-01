864526c03c990d32468dd803198244b6985053ce
Nicolas Crausaz <n.crausaz99@gmail.com>
2022-11-01 13:25:44 +0100

    Added methods

--- src/main/java/ch/heigvd/amt/team10/cloud/AWSDataObjectHelper.java
+++ src/main/java/ch/heigvd/amt/team10/cloud/AWSDataObjectHelper.java
@@ -1,4 +1,8 @@
 package ch.heigvd.amt.team10.cloud;
 
 public class AWSDataObjectHelper implements IDataObjectHelper {
+    @Override
+    public void create(String objectName) {
+        // TODO: create object in s3 bucket
+    }
 }
--- src/main/java/ch/heigvd/amt/team10/cloud/AWSLabelDetectorHelper.java
+++ src/main/java/ch/heigvd/amt/team10/cloud/AWSLabelDetectorHelper.java
@@ -1,4 +1,9 @@
 package ch.heigvd.amt.team10.cloud;
 
 public class AWSLabelDetectorHelper implements ILabelDetector {
+
+    @Override
+    public String execute(String imageUri, int[] params) {
+        return "";
+    }
 }
